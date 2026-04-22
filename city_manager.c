ab17a9333ea294673d3a627c7c65f5fbe5482748
alexandra-dragos <dragosalexandra1234@gmail.com>
2026-04-22 16:58:19 +0300

    view si remove

--- city_manager.c
+++ city_manager.c
@@ -4,6 +4,8 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 typedef struct{
   int report_id;
@@ -47,11 +49,11 @@ int main(int argc, char* argv[])
   if(district_name!=NULL)
     {
       struct stat st={0};
-      char path[512];
-      sprintf(path, "%s", district_name);
+      char report_file[512];
+      sprintf(report_file, "%s/reports.dat", district_name);
 
       //daca districyul nu exista il cream
-      if(stat(path, &st)==-1)
+      if(stat(district_name, &st)==-1)
 	{
 	  if(strcmp(role, "manager")!=0)
 	    {
@@ -61,17 +63,13 @@ int main(int argc, char* argv[])
 	  printf("Districtul nou: %s\n", district_name);
 	  mkdir(district_name, 0750);
 
-	  char file_path[512];
-
-	  //formez cele 3 fisierle pentru fiecare district 
-	  //reports.dat
-	  sprintf(file_path, "%s/reports.dat", district_name);
-	  FILE* f1=fopen(file_path, "wb");
-	  if(f1!=NULL)
-	    fclose(f1);
-	  chmod(file_path, 0664);
+	  FILE* f_init=fopen(report_file, "wb");
+	  if(f_init !=NULL)
+	    fclose(f_init);
+	  chmod(report_file, 0664);
 
 	  //district.cfg
+	  char file_path[512];
 	  sprintf(file_path, "%s/district.cfg", district_name);
 	  FILE* f2=fopen(file_path, "w");
 	  if(f2!=NULL)
@@ -92,16 +90,13 @@ int main(int argc, char* argv[])
 	  chmod(file_path, 0644);
 	  
 	}
-      //adaug raportul in reports.dat
-      char report_file[512];
-      sprintf(report_file, "%s/reports.dat", district_name);
-
       FILE* file=fopen(report_file, "ab");
       if(file==NULL)
 	{
-	  printf("nu s-a putut deschide fisierul\n");
+	  printf("nu s-a putut accesa continutul raportului\n");
 	  return 1;
 	}
+     
       Report rap_nou;
       printf("\nADAUGARE RAPORT NOU:\n");
       
@@ -111,7 +106,7 @@ int main(int argc, char* argv[])
       strncpy(rap_nou.inspector_name, user, 50);
 
       printf("categorie:");
-      scanf("%50s", rap_nou.issue_category);
+      scanf("%29s", rap_nou.issue_category);
 
       printf("severitate:");
       scanf("%d", &rap_nou.severity);
@@ -122,17 +117,24 @@ int main(int argc, char* argv[])
       scanf("%f", &rap_nou.latitude);
 
       rap_nou.timestamp=time(NULL);
+      while(getchar()!='\n');
 
       printf("descriere:");
-      getchar();
       fgets(rap_nou.description_text, 250, stdin);
       rap_nou.description_text[strcspn(rap_nou.description_text, "\n")]='\0';
 
-      fwrite(&rap_nou, sizeof(Report), 1, file);
-      fclose(file);
+      //scriere in fisier
+      if(fwrite(&rap_nou,sizeof(Report), 1, file)==1)
+	{
+	  printf("raportul %d a fost adaugat in continuarea fisierului\n", rap_nou.report_id);
+	}
+      else{
+	printf("nu s-a putut adauga raportul\n");
+      }
 
+      fclose(file);
       chmod(report_file, 0664);
-      printf("raport id %d adaugat cu succes de %s\n", rap_nou.report_id, user);
+     
     }
 
   for(int i=0; i<argc; i++)
@@ -185,7 +187,120 @@ int main(int argc, char* argv[])
     }
   
 
-  
+  for(int i=0; i<argc; i++)
+    {
+      if(strcmp(argv[i], "--view")==0 && (i+2<argc))
+	{
+	  char* district_id=argv[i+1];
+	  int target_id=atoi(argv[i+2]);
+	  char cale[512];
+	  sprintf(cale, "%s/reports.dat", district_id);
+
+	  struct stat file_info;
+	  if(stat(cale, &file_info)==-1)
+	    {
+	      printf("districtul %s nu exista sau nu are rapoarte\n", district_id);
+	      continue;
+	    }
+	  FILE* file=fopen(cale, "rb");
+	  if(file==NULL)
+	    {
+	      printf("nu s-a putut deschide fisierul\n");
+	      continue;
+	    }
+	  printf("\ninformatii fisier:%s\n",cale);
+	  printf("marimea:%ld\n", file_info.st_size);
+	  printf("Permisiuni:\n");
+	  printf((file_info.st_mode & S_IRUSR) ? "r" : "-");
+	  printf((file_info.st_mode & S_IWUSR) ? "w" : "-");
+	  printf((file_info.st_mode & S_IXUSR) ? "x" : "-");
+	  printf((file_info.st_mode & S_IRGRP) ? "r" : "-");
+	  printf((file_info.st_mode & S_IWGRP) ? "w" : "-");
+	  printf((file_info.st_mode & S_IXGRP) ? "x" : "-");
+	  printf((file_info.st_mode & S_IROTH) ? "r" : "-");
+	  printf((file_info.st_mode & S_IWOTH) ? "w" : "-");
+	  printf((file_info.st_mode & S_IXOTH) ? "x" : "-");
+
+	  
+	  Report temp;
+	  while(fread(&temp, sizeof(Report), 1, file)==1)
+	    {
+	      if(temp.report_id==target_id)
+		{
+		  printf("report id: %d\n", temp.report_id);
+		  printf("nume inspector: %s\n", temp.inspector_name);
+		  printf("tipul problemei: %s\n", temp.issue_category);
+		  printf("severitate: %d\n", temp.severity);
+		  printf("descriere: %s\n", temp.description_text);
+		  break;
+		  
+		}
+	    }
+	  fclose(file);
+	  i=i+2;
+	}
+    }
+
+  for(int i=0; i<argc; i++)
+    {
+      if(strcmp(argv[i], "--remove_report")==0 && (i+2<argc))
+	{
+	  char* district_target=argv[i+1];
+	  int target_id=atoi(argv[i+2]);
+
+	  //verificare rol, doar managerul poate sterge
+	  if(role==NULL || strcmp(role, "manager")!=0)
+	    {
+	      printf("doar managerul poate sterge rapoarte\n");
+	      continue;
+	    }
+	  char cale[512];
+	  sprintf(cale, "%s/reports.dat", district_target);
+	  int f=open(cale, O_RDWR);
+	  if(f<0)
+	    {
+	      printf("nu s-a putut deschide fisierul\n");
+	      continue;
+	    }
+	  Report temp;
+	  int gasit=0;
+	  long offset_sters=0;
+	  //caut raportul si salvez poz
+	  while(read(f, &temp, sizeof(Report))==sizeof(Report))
+	    {
+	      if(temp.report_id==target_id)
+		{
+		  gasit=1;
+		  offset_sters=lseek(f, 0, SEEK_CUR)-sizeof(Report);
+		  break;
+		}
+	    }
+	  if(gasit==1)
+	    {
+	      //deplasez rapoartele de dupa cel de sters cu o pozitie in sus
+	      long offset_urmator=offset_sters+sizeof(Report);
+	      Report aux;
+
+	      while(lseek(f, offset_urmator, SEEK_SET)>=0 && read(f, &aux, sizeof(Report))==sizeof(Report))
+		{
+		  lseek(f, offset_sters, SEEK_SET);
+		  write(f, &aux, sizeof(Report));
+		  //avansez pozitiile
+		  offset_sters+=sizeof(Report);
+		  offset_urmator+=sizeof(Report);
+		}
+	      struct stat st;
+	      fstat(f, &st);
+	      ftruncate(f, st.st_size-sizeof(Report));
+
+	      printf("raportul %d a fost sters\n", target_id);
+	    }
+	  else{
+	    printf("raportul %d nu a fost gasit\n", target_id);
+	  }
+	  close(f);
+	}
+    }
   
   
   
