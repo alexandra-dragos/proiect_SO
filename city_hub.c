ea25f810b79988cb4af5061846aae6db8a86aea3
alexandra-dragos <dragosalexandra1234@gmail.com>
2026-05-17 22:30:35 +0300

    consola interactiva

---
+++ city_hub.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int citeste_linia(int fd, char* buffer, int max_len)
+{
+  int i=0;
+  char c;
+  while(i<max_len-1)
+    {
+      int n=read(fd, &c, 1);
+      if(n<=0)
+	{
+	  if(i==0)
+	    return 0;
+	  break;
+	}
+      buffer[i]=c;
+      i++;
+      if(c=='\n')
+	break;
+    }
+  buffer[i]='\0';
+  return i;
+}
+
+int main(void)
+{
+  char comanda[255];
+
+  while(1)
+    {
+      printf("city_hub ");
+      fflush(stdout);
+
+      if(fgets(comanda, sizeof(comanda),stdin)==NULL)
+	{
+	  break;
+	}
+      comanda[strcspn(comanda, "\n")]='\0';
+
+      if(strcmp(comanda, "exit")==0)
+	{
+	  printf("inchidere city_hub\n");
+	  break;
+	}
+      else if(strcmp(comanda, "start_monitor")==0)
+	{
+	  pid_t pid_hub_mon=fork();
+
+	  if(pid_hub_mon<0)
+	    {
+	      printf("nu s-a putut crea procesul hub_mon\n");
+	    }
+	  else if(pid_hub_mon==0)
+	    {
+	      int pfd[2];
+	      if(pipe(pfd)<0)
+		{
+		  perror("hub_mon: eroare la crearea pipe-ului\n");
+		  exit(-1);
+		}
+	      pid_t pid_monitor=fork();
+	      if(pid_monitor<0)
+		{
+		  perror("hub_mon: eroare la fork pentr monitor\n");
+		  exit(-1);
+		}
+	      else if(pid_monitor==0)
+		{
+		  //processul copil a lui hub_mon
+		  dup2(pfd[1], STDOUT_FILENO);
+
+		  close(pfd[0]);
+		  close(pfd[1]);
+
+		  execlp("./monitor_reports", "monitor_reports",NULL);
+		  perror("eroare la execlp pentru monitor_reports\n");
+		  exit(-1);
+		}
+	      else
+		{
+		  close(pfd[1]);
+
+		  char buffer[1000];
+		  int monitor_running=1;
+		  while(citeste_linia(pfd[0], buffer, sizeof(buffer))>0)
+		    {
+		      if(strncmp(buffer, "EROARE:", 7)==0)
+			{
+			  printf("\nhub_mon notitficare: Monitorul nu porneste:  %s", buffer+7);
+			  monitor_running=0;
+			}
+		      else if(strncmp(buffer, "INFO:", 5)==0)
+			{
+			  printf("\nhub_mon LOG: %s", buffer+5);
+			}
+		      else
+			{
+			  printf("hub_mon necunoscut: %s", buffer);
+			}
+		      fflush(stdout);
+		    }
+		  close(pfd[0]);
+
+		  int status;
+		  waitpid(pid_monitor, &status, 0);
+		  if(!monitor_running)
+		    {
+		      printf("hub_mon: executie anulata din cauza unei instante care exista deja\n");
+		    }
+		  else
+		    {
+		      printf("hub_mon: procesul monitor s-a terminat\n");
+		    }
+		  fflush(stdout);
+		  exit(0);
+		}
+	    }
+	  else
+	    {
+	      printf("comanda start_monitor a fost trimisa in fundal\n");
+	    }
+	}
+      else if(strlen(comanda)>0)
+	{
+	  printf("Comanda necunoscuta: %s\n", comanda);
+	}
+      
+    }
+  return 0;
+}
